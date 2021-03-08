7c817f1eca9f48621b58a1b225d1ddbbe49d0f30
Veronica Stork <zealous.anemone@gmail.com>
2021-03-08 18:35:51 -0500

    Conect to db to retrieve dreams. Very basic functionality. No styling yet, or integrated nfo about user

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,10 +14,10 @@ const Header = () => {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           {user ? <><Nav.Link><Link href="/add"><a>Add Dream</a></Link></Nav.Link>
-          <Nav.Link><Link href="/diary">Dream Diary</Link></Nav.Link></> : <><Nav.Link><Link href="/about">About</Link></Nav.Link><Nav.Link><Link href="/signup">Sign up</Link></Nav.Link>  </>}
+          <Nav.Link><Link href="/diary"><a>Dream Diary</a></Link></Nav.Link></> : <><Nav.Link><Link href="/about"><a>About</a></Link></Nav.Link><Nav.Link><Link href="/signup"><a>Sign up</a></Link></Nav.Link>  </>}
         </Nav>
         {/* conditionally render my acct or login */}
-        <Nav.Link>{user ? <Link href="/account">My Account</Link> : <Link href="/login">Login</Link>}</Nav.Link>
+        <Nav.Link>{user ? <Link href="/account"><a>My Account</a></Link> : <Link href="/login"><a>Login</a></Link>}</Nav.Link>
       </Navbar.Collapse> 
       </Navbar>
   )
--- pages/diary.js
+++ pages/diary.js
@@ -1,13 +1,33 @@
-import Head from 'next/head';
+import { PrismaClient } from '@prisma/client';
 import Header from '../components/Header';
 
-const Diary = () => {
+const Diary = ({ entries }) => {
   return (
-    <div>
+    <>
       <Header />
       <h1>Diary</h1>
-    </div>
+      <ul>
+        {entries.map((entry) => (
+          <li>{entry.dream_content}</li>
+        ))}
+      </ul>
+    </>
   );
+};
+
+const prisma = new PrismaClient();
+export const getStaticProps = async () => {
+  const entries = await prisma.dreams.findMany({
+    where: { user_id: 1 },
+    select: {
+      dream_content: true,
+    },
+  });
+  return {
+    props: {
+      entries,
+    },
+  };
 };
 
 export default Diary;
