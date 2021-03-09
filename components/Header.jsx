24a33cd28dc98a6ad3adf7207f1df24400ecfa1f
Veronica Stork <zealous.anemone@gmail.com>
2021-03-08 20:19:00 -0500

    Adds Next Auth

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,21 +3,25 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Link from 'next/link';
 import { useContext } from 'react';
+import { signIn, signOut, useSession } from 'next-auth/client';
+
 import UserContext from './UserContext';
 
 const Header = () => {
   const { user, setUser } = useContext(UserContext);
+  const [session, loading] = useSession();
+
   return (
     <Navbar bg='light' expand='lg'>
       <Navbar.Brand>Electric Sheep</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          {user ? <><Nav.Link><Link href="/add"><a>Add Dream</a></Link></Nav.Link>
+          {session ? <><Nav.Link><Link href="/add"><a>Add Dream</a></Link></Nav.Link>
           <Nav.Link><Link href="/diary"><a>Dream Diary</a></Link></Nav.Link></> : <><Nav.Link><Link href="/about"><a>About</a></Link></Nav.Link><Nav.Link><Link href="/signup"><a>Sign up</a></Link></Nav.Link>  </>}
         </Nav>
         {/* conditionally render my acct or login */}
-        <Nav.Link>{user ? <Link href="/account"><a>My Account</a></Link> : <Link href="/login"><a>Login</a></Link>}</Nav.Link>
+       {session ?  <Nav.Link><Link href="/account"><a>My Account</a></Link></Nav.Link> : <button onClick={()=> signIn()}>Login</button>}
       </Navbar.Collapse> 
       </Navbar>
   )
