92edfbf5e31a325d7f86f89176fd04a52425c2ef
Veronica Stork <zealous.anemone@gmail.com>
2021-03-10 18:21:16 -0500

    add support for FB login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,6 +4,10 @@ import Providers from 'next-auth/providers';
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
+    Providers.Facebook({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    }),
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
