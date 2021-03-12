5f130db91319f03dfb1c112557178e6b029179f6
Veronica Stork <zealous.anemone@gmail.com>
2021-03-12 12:54:10 -0500

    testing session object

--- pages/account.js
+++ pages/account.js
@@ -3,6 +3,7 @@ import { getSession } from 'next-auth/client';
 
 async function getUser() {
   const session = await getSession();
+  console.log(session);
   return session;
 }
 const Account = () => {
@@ -13,7 +14,7 @@ const Account = () => {
       <h1>Account</h1>
       <p>On this page, you will be able to view & change acct info</p>
       <ul>
-        <li>{user.email}</li>
+        <li>{user.user.email}</li>
         <li>preferences re: tracking</li>
         <li>Whether acct is publically available</li>
       </ul>
