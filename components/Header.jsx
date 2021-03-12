5bdc4aebd6fe103e2b09aeb633c00168f0e13134
Veronica Stork <zealous.anemone@gmail.com>
2021-03-12 12:35:36 -0500

    adds user info to acct page

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,10 +14,10 @@ const Header = () => {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           {session ? <><Nav.Link><Link href="/add"><a>Add Dream</a></Link></Nav.Link>
-          <Nav.Link><Link href="/diary"><a>Dream Diary</a></Link></Nav.Link></> : <><Nav.Link><Link href="/about"><a>About</a></Link></Nav.Link><Nav.Link><Link href="/signup"><a>Sign up</a></Link></Nav.Link>  </>}
+          <Nav.Link><Link href="/diary"><a>Dream Diary</a></Link></Nav.Link></> : <Nav.Link><Link href="/about"><a>About</a></Link></Nav.Link>}
         </Nav>
         {/* conditionally render my acct or login */}
-       {session ? <button onClick={()=> signOut()}>Sign Out</button>: <button onClick={()=> signIn()}>Sign in</button>}
+       {session ? <><Nav.Link><Link href="/account"><a>My Account</a></Link></Nav.Link><button onClick={()=> signOut()}>Sign Out</button></> : <button onClick={()=> signIn()}>Sign in</button>}
       </Navbar.Collapse> 
       </Navbar>
   )
--- pages/account.js
+++ pages/account.js
@@ -1,13 +1,19 @@
 import Header from '../components/Header';
+import { getSession } from 'next-auth/client';
 
+async function getUser() {
+  const session = await getSession();
+  return session;
+}
 const Account = () => {
+  const user = getUser();
   return (
     <div>
       <Header />
       <h1>Account</h1>
       <p>On this page, you will be able to view & change acct info</p>
       <ul>
-        <li>email address</li>
+        <li>{user.email}</li>
         <li>preferences re: tracking</li>
         <li>Whether acct is publically available</li>
       </ul>
